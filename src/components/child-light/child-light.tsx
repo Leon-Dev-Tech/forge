95200cf71560ebfc5c80592207d69ce2b0ebb254
Leon.Shi <leon.tech.dev@gmail.com>
2026-05-21 01:41:50 +0800

    last resort111

--- src/components/child-light/child-light.tsx
+++ src/components/child-light/child-light.tsx
@@ -33,7 +33,7 @@ export class ChildLight {
   render() {
     return (
       <div style={{ border: '2px dashed blue', padding: '10px' }}>
-        <p>[Child: Light DOM]</p>
+        <p>[Child: Light DOM 11111111]</p>
         <img 
           src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" 
           style={{ width: '80px', height: '80px' }} 
